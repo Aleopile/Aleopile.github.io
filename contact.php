12689c93e98ce0474c0a070dfb9f2e2114808cff
kids-bytes <send2arohan@gmail.com>
2013-11-22 16:53:14 -0800

    added more social media

--- contact.php
+++ contact.php
@@ -60,10 +60,10 @@
       <p><i class="fa fa-envelope-o"></i> <abbr title="Email">E</abbr>: <a href="rohan@aleopile.com">rohan@aleopile.com</a></p>
       <p><i class="fa fa-clock-o"></i> <abbr title="Hours">H</abbr>: Monday - Saturday: 3:30 PM to 8:00 PM PST</p>
       <ul class="list-unstyled list-inline list-social-icons">
-        <li class="tooltip-social facebook-link"><a href="#facebook-page" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="fa fa-facebook-square fa-2x"></i></a></li>
-        <li class="tooltip-social linkedin-link"><a href="#linkedin-company-page" data-toggle="tooltip" data-placement="top" title="LinkedIn"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
-        <li class="tooltip-social twitter-link"><a href="#twitter-profile" data-toggle="tooltip" data-placement="top" title="Twitter"><i class="fa fa-twitter-square fa-2x"></i></a></li>
-        <li class="tooltip-social google-plus-link"><a href="#google-plus-page" data-toggle="tooltip" data-placement="top" title="Google+"><i class="fa fa-google-plus-square fa-2x"></i></a></li>
+        <li class="tooltip-social facebook-link"><a href="https://www.facebook.com/aleopile" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+        <!-- <li class="tooltip-social linkedin-link"><a href="#linkedin-company-page" data-toggle="tooltip" data-placement="top" title="LinkedIn"><i class="fa fa-linkedin-square fa-2x"></i></a></li> -->
+        <li class="tooltip-social twitter-link"><a href="https://twitter.com/Aleopile" data-toggle="tooltip" data-placement="top" title="Twitter"><i class="fa fa-twitter-square fa-2x"></i></a></li>
+        <li class="tooltip-social google-plus-link"><a href="https://plus.google.com/102603553828858438695/posts" data-toggle="tooltip" data-placement="top" title="Google+"><i class="fa fa-google-plus-square fa-2x"></i></a></li>
       </ul>
     </div>
 
@@ -75,20 +75,61 @@
 
   <hr>
 
-  <footer>
-    <div class="row">
-      <div class="col-lg-12">
-        <p>Copyright &copy; Company 2013</p>
-      </div>
+  <div class="row" style="padding: 15px;">
+      <div class="g-plusone" data-size="medium"></div>
+      <a href="https://twitter.com/aleopile" class="twitter-follow-button" data-show-count="true" data-lang="en">Follow @aleopile</a>
+      <div class="fb-like" data-href="https://facebook.com/aleopile" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
     </div>
-  </footer>
+    <!-- FOOTER -->
+    <footer>
+      <!-- <p class="pull-right"><a href="#">Back to top</a></p> -->
+      <p class="pull-right"><a href="#"> &middot; Back to top</a></p>
+      <p>&copy; 2013 Aleopile &middot; <!-- <a href="#">Privacy</a> &middot; <a href="#">Terms</a> --></p>
+    </footer>
 
 </div><!-- /.container -->
 <!-- Bootstrap core JavaScript -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="js/jquery.js"></script>
-<script src="js/bootstrap.js"></script>
-<script src="js/modern-business.js"></script>
+<div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
 
+  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+  <!-- Place this tag after the last +1 button tag. -->
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+  <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+  <script src="js/jquery.lazyload.min.js" type="text/javascript"></script>
+  <script type="text/javascript" charset="utf-8">
+  $(function() {
+     $("img.lazy").lazyload({
+         effect : "fadeIn"
+     });
+  });
+  </script>
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-45380875-1', 'aleopile.com');
+    ga('send', 'pageview');
+
+  </script>
 </body>
 </html>
